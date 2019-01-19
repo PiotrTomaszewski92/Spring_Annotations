91070b11df6eb0b0f16be708c36075ba15232643
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-01-19 20:17:58 +0100

    implement data from external file properties

--- src/main/java/SpringConfiguration/SwimCoach.java
+++ src/main/java/SpringConfiguration/SwimCoach.java
@@ -1,4 +1,38 @@
 package SpringConfiguration;
 
-public class SwimCoach {
+import org.springframework.beans.factory.annotation.Value;
+import springdemo.Coach;
+import springdemo.FortuneService;
+
+public class SwimCoach implements Coach {
+
+    private FortuneService sadFortuneService;
+
+    @Value("${my.email}")
+    private String email;
+
+    @Value("${my.name}")
+    private String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SwimCoach(FortuneService sadFortuneService) {
+        this.sadFortuneService = sadFortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warm up ";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return sadFortuneService.getFortune();
+    }
 }
--- src/main/java/SpringConfiguration/SwimJavaConfigDemo.java
+++ src/main/java/SpringConfiguration/SwimJavaConfigDemo.java
@@ -1,16 +1,26 @@
 package SpringConfiguration;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springdemo.Coach;
 
-public class JavaConfigDemo {
+public class SwimJavaConfigDemo {
+    public static void main(String[] args) {
+
 
     //Read Spring configuration File
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
     //get a bean from Spring container
-
+    Coach theCoach = context.getBean("swimCoach",Coach.class);
+    SwimCoach swimCoach = context.getBean("swimCoach",SwimCoach.class);
 
     //call a method from a bean
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+        System.out.println(swimCoach.getEmail()+" -> "+swimCoach.getName());
 
     //close the context
+        context.close();
+
+    }
 }
