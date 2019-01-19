48ad4162bf6e32c9f9e9f3070c8b7d8c7f70f0e5
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-01-19 18:16:40 +0100

    add init and destroy method

--- src/main/java/springdemo/TennisCoach.java
+++ src/main/java/springdemo/TennisCoach.java
@@ -5,13 +5,28 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component //default bean ID
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
     @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
+
+    //define my init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println(">>TennisCoach: inside of doMyStartupStuff");
+    }
+
+    //define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">>TennisCoach: inside of doMyCleanupStuff");
+    }
+
 
         //define a constructor
 //    @Autowired
