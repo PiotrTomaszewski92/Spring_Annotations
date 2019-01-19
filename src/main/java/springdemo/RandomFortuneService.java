10ae7900abc1ecd0fafa49e91b3dabe9eb0f9487
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-01-19 16:58:20 +0100

    add implementation for RandomFortuneService class

--- src/main/java/springdemo/RandomFortuneService.java
+++ src/main/java/springdemo/RandomFortuneService.java
@@ -2,11 +2,22 @@ package springdemo;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService{
 
+    private String[] data ={
+            "Beware of the wolf in sheep's clothing",
+            "Diligence is the mother of good luck",
+            "The journey is the reward"
+    };
+    private Random myRandom = new Random();
+
     @Override
     public String getFortune() {
-        return null;
+        int index = myRandom.nextInt(data.length);
+        return data[index];
+
     }
 }
